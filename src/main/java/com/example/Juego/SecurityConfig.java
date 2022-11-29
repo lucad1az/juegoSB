19086b46c55fb8488496b632dc740494b009e812
Luca <luca-diaz@live.com>
2022-11-29 00:04:14 -0300

    commit inicial

---
+++ src/main/java/com/example/Juego/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.example.Juego;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/", "/styles/**", "/h2/**").permitAll();
+
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+
+        return http.build();
+    }
+
+}
